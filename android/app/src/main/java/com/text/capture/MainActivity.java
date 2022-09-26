1dd53e96fe0234efa6524ea12981224abef57da2
Lóránd Véger <lorand.veger@movilitas.com>
2022-09-26 11:05:42 +0200

    sample for testing

---
+++ android/app/src/main/java/com/text/capture/MainActivity.java
@@ -0,0 +1,5 @@
+package com.text.capture;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import 'scandit-capacitor-datacapture-core';
+import 'scandit-capacitor-datacapture-parser';
+import 'scandit-capacitor-datacapture-text';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const { ScanditCaptureCorePlugin } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -8,5 +15,19 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  async initTextCapture(): Promise<void> {
+    console.log('Init started');
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const Scandit = await ScanditCaptureCorePlugin.initializePlugins();
+    console.log('Init finished');
+    // eslint-disable-next-line max-len
+    const context = Scandit.DataCaptureContext.forLicenseKey("-- ENTER YOUR SCANDIT LICENSE KEY HERE --");
+
+    const camera = Scandit.Camera.default;
+    context.setFrameSource(camera);
+    console.log('Reached main check point');
+
+  }
 
 }
